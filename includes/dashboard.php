b1353c80fa6581ce169a6b058af2699c06fe9aab
Shan8 <shanaczanecruzat@gmail.com>
2025-05-21 12:20:03 +0800

    dashboard UI

--- includes/dashboard.php
+++ includes/dashboard.php
@@ -1,1 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+  <meta charset="UTF-8">
+  <title>Sidebar Menu</title>
+  <link rel="stylesheet" href="../assets/style.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet"> <!-- Icons -->
+</head>
+
+<body>
+  <div class="sidebar">
+    <ul>
+      <li class="active"><i class="bi bi-map"></i> <span>Map</span></li>
+      <li><i class="bi bi-cash"></i> <span>Cash register</span></li>
+      <li><i class="bi bi-shop"></i> <span>Shop</span></li>
+      <li><i class="bi bi-printer"></i> <span>Printing module</span></li>
+      <li><i class="bi bi-file-earmark-text"></i> <span>Contents</span></li>
+      <li><i class="bi bi-graph-up"></i> <span>Statistics</span></li>
+      <li><i class="bi bi-grid-3x3-gap"></i> <span>Dashboard</span></li>
+      <li><i class="bi bi-geo-alt"></i> <span>Zones and rates</span></li>
+      <li><i class="bi bi-people"></i> <span>Users</span></li>
+      <li><i class="bi bi-megaphone"></i> <span>News</span></li>
+      <li><i class="bi bi-gear"></i> <span>Settings</span></li>
+    </ul>
+  </div>
+</body>
+
+</html>
