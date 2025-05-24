15a7d2ddff2ea85571f05946a741b07dcf33e4a8
Shan8 <shanaczanecruzat@gmail.com>
2025-05-24 14:35:53 +0800

    change format

--- computer.php
+++
@@ -1,6 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head> Hello World
-    <meta charset="UTF-8">
-    <title>Computer Management</title>
-  </head>
---
+++ dashboard/computer.php
@@ -0,0 +1,12 @@
+<?php
+$title = "Computers";
+include 'include/index.php'; // remove the "..", because it's already in the root folder
+?>
+
+<h1>Computer Page</h1>
+<p>This is a simple placeholder for the Computers section.</p>
+
+</div> <!-- closes main-content -->
+</body>
+
+</html>
