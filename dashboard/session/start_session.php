59d086e9282488507ff9be4cd95751539623cbc5
Shan8 <shanaczanecruzat@gmail.com>
2025-05-24 22:40:49 +0800

    i dunnor

--- dashboard/session/start_session.php
+++ dashboard/session/start_session.php
@@ -1,6 +1,20 @@
-<?php include('../../include/index.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
 
-<div style="margin-left: 260px; padding: 20px;">
-  <h1>Start Session</h1>
-  <p>Start timing customer session</p>
-</div>
+<head>
+  <meta charset="UTF-8">
+  <title>End Session</title>
+</head>
+
+<body>
+
+  <?php include('../../include/index.php'); ?>
+
+  <div style="margin-left: 260px; padding: 20px;">
+    <h1>StartnnSession</h1>
+    <p>Here you can start sessions of computers</p>
+  </div>
+
+</body>
+
+</html>
